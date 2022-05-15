5b1d1c3cedf871b4af61f44ae8437384dc57adc4
jrgluis.mejiad <jrgluis.mejiad@gmail.com>
2022-05-15 13:57:31 -0400

    creacion del cv con css

--- 1ra Tarea - EjerciciosBasicos/ejercicio_8.js
+++ 1ra Tarea - EjerciciosBasicos/ejercicio_8.js
@@ -11,26 +11,26 @@ prompt.get(['x'], function(err, result){
 	
 	let residuo;
 	
-	if (numero > 3) {
+	//if (numero > 3 &&) {
 		
-		for(let i = numero - 1; i >= 2; i--){
+		for(let i = numero - 1; i = 2; i--){
 			
 			residuo = numero % i;
 			//console.log(i + ', ' + residuo);
 			
-			if(residuo == 0){
-				console.log('El numero ' + numero + ' no es primo');
+			if(residuo != 0){
+				console.log('El numero ' + numero + ' es primo');
 				break;
 			}else{
-				console.log('El numero ' + numero + ' es primo');
+				console.log('El numero ' + numero + ' no es primo');
 				break;
 			}
 		}
 		
-	}else{
+	/*else{
 		
 		console.log('El numero ' + numero + ' es primo');
 		
-	}
+	}*/
 	
 });
